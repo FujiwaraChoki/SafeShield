2c8bbb48c53491a9b67bcc807ba04e6e2112c562
Sami <78088687+FujiwaraChoki@users.noreply.github.com>
2023-01-10 21:56:41 +0100

    Added email functionality (with new line characters)

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 const checkHasUploaded = (id) => {
-    // Get the file input element by id
-    const fileInput = document.getElementById(id);
-    // Get the file input element's value
-    const fileInputValue = fileInput.value;
-    // Check if the file input element has a value
-    if (fileInputValue) {
-        // If the file input element has a value, return true
-        return true;
-    } else {
-        // If the file input element does not have a value, return false
-        return false;
-    }
-}
-
-const getAppointmentData = (a_id) => {
-    let appointments = {
-        1: {
-            name: "John Doe",
-            email: "johndoe@example.com",
-            phone: "1234567890",
-            date: "2020-01-01",
-            time: "12:00",
-        },
-        2: {
-            name: "Jane Doe",
-            email: "janedoe@example.com",
-            phone: "0987654321",
-            date: "2020-01-02",
-            time: "13:00",
-        },
-    }
-
-    return appointments[a_id];
+  // Get the file input element by id
+  const fileInput = document.getElementById(id);
+  // Get the file input element's value
+  const fileInputValue = fileInput.value;
+  // Check if the file input element has a value
+  if (fileInputValue) {
+    // If the file input element has a value, return true
+    return true;
+  } else {
+    // If the file input element does not have a value, return false
+    return false;
+  }
 };
+
+const getRandomFaces = () => {
+  faces = [];
+  // Generate a random number between 1 and 10000
+  const randomNumber = Math.floor(Math.random() * 10000) + 1;
+  // Change the image src to a new random face
+  faces.push(`https://thispersondoesnotexist.com/image?rand=${randomNumber}`);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sendMailButton = document.querySelector("#send-mail-button");
+  sendMailButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Get the name input element
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    const mailToLink = `mailto:sami@samihindi.com?subject=Message from ${name}&body=Site: Safeshield %0D%0AName: ${name}%0D%0AE-Mail: ${email}%0D%0A%0D%0AMessage:%0D%0A${message}`;
+    window.location.href = mailToLink;
+  });
+});
